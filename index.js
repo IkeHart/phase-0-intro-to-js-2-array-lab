edc093044725523932188e462cea3544dc6f5b84
Ike Reed <ikereed@protonmail.com>
2022-09-27 15:22:08 -0500

    commit tree 76a9538d2b325e72ebac882e4122c840de6eb84b

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+
+
+cats.push(name);
+}
+function destructivelyPrependCat(name){
+
+
+    cats.unshift(name);
+    }
+function destructivelyRemoveLastCat(){
+
+
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+        
+        
+    cats.shift();
+}
+function appendCat(name){
+
+
+cats.slice(0, name);
+}
+
+function prependCat(name){
+                
+                
+    cats.slice(name);
+}
+function removeLastCat(){
+                
+                
+    cats.slice(2, 4);
+}
+function removeFirstCat(){
+                
+                
+    cats.slice(2);
+}
+
+
+    
